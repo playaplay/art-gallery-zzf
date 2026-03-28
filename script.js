98d3e85b92ffb73f94a901aea191d1178dc9c30d
playaplay <46336059+playaplay@users.noreply.github.com>
2026-03-28 09:53:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 const langContent = {
   zh: {
     name: '章肇方',
-    titles: '四川美术协会副会长 / 中国当代艺术家', // 在这里填入中文头衔
-    desc: '二〇二六 · 线上作品展',
+    titles:
+      '1962年12月出生  中国水彩画家<br>四川省美术家协会水彩艺术委员会副主任 <br> 天府新区水彩专业委员会主任', // 在这里填入中文头衔
+    desc: '在德国等国内外举办过多次个展。<br>主要个展包括：2024年在德国锡根举办水彩双人展、2016年在德国鲁本斯文化交流中心举办水彩个展、<br>2014年受中德友好协会邀请在德国举办个展、2013年在成都蓝顶艺术区举办“天圆地方”个展等。<br>2007年受柏林艺术家联盟邀请举办个展。<br>作品发表于《中国水彩》专题号（2021年）以及《艺术界》《美术观潮》等美术专业杂志。<br>2005年由江苏美术出版社出版作品集《章肇方水彩》。',
     contactLabel: '联系方式 / CONTACT',
     footer: '艺术，是灵魂的呼吸。',
   },
   jp: {
-    name: '先生の名前',
-    titles: '四川美術家協会副会長 / 中国現代アーティスト', // 在这里填入日文头衔
-    desc: '二〇二六 · オンライン作品展',
+    name: '章肇方（しょう ちょうほう）',
+    titles:
+      '1962年12月生まれ  中国の水彩画家<br>四川省美術家協会水彩画芸術委員会副主任<br>天府新区水彩専門委員会主任', // 在这里填入日文头衔
+    desc: 'ドイツをはじめ国内外で個展を多数開催。<br>主な個展に、2024年ドイツ・ジーゲンにて水彩二人展、2016年ドイツ・ルーベンス文化交流センター水彩個展、<br>2014年中独友好協会招聘によるドイツ個展、2013年成都ブルートップ芸術区にて「天円地方」個展など。<br>2007年にはベルリン芸術家連盟の招待により個展を開催。<br>作品は『中国水彩』特集号（2021年）をはじめ、『芸術界』『美術観潮』などの美術専門誌に掲載。<br>2005年には江蘇美術出版社より作品集『章肇方水彩』を出版。',
     contactLabel: 'お問い合わせ / CONTACT',
     footer: '芸術とは、魂の呼吸である。',
   },
@@ -24,7 +26,7 @@ function setLang(lang) {
   if (!data) return;
 
   document.getElementById('artist-name').innerText = data.name;
-  document.getElementById('artist-desc').innerText = data.desc;
+  document.getElementById('artist-desc').innerHTML = data.desc;
   document.getElementById('footer-name').innerText = data.footer;
   document.getElementById('contact-label').innerText = data.contactLabel;
 
@@ -68,7 +70,7 @@ function copyWeChat(id) {
   alert(
     document.documentElement.lang === 'jp'
       ? 'IDをコピーしました'
-      : '微信号已复制'
+      : '微信号已复制',
   );
 }
 
@@ -119,7 +121,7 @@ function init() {
       touchStartX = e.changedTouches[0].screenX;
       touchStartY = e.changedTouches[0].screenY;
     },
-    { passive: true }
+    { passive: true },
   );
 
   lbContent.addEventListener(
@@ -136,7 +138,7 @@ function init() {
         changeImage(dx > 0 ? -1 : 1);
       }
     },
-    { passive: true }
+    { passive: true },
   );
 }
 
